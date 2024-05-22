ec3ce5fe659eb53637a882be702f71cb027b34b8
estrella <epopo2@illinois.edu>
2024-05-22 15:52:42 -0400

    add react chapter 8 chapter 38

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,26 @@
-/* eslint-disable react/prop-types */
-import { createContext, useContext, useState, useEffect } from 'react';
-import Column from './Column';
+import NavBar from './components/NavBar';
+import { Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import One from './components/One';
+import Two from './components/Two';
+import Three from './components/Three';
+
 import './styles.css';
-
-export const BoardContext = createContext();
-
-const TaskBoardProvider = function() {
-  const [tasks, setTasks] = useState([
-    { id: 1, title: 'Task 1', status: 'todo' },
-    { id: 2, title: 'Task 2', status: 'todo' },
-    { id: 3, title: 'Task 3', status: 'todo' },
-  ]);
-
-  const moveTask = (taskId, newStatus) => {
-    const updatedTasks = tasks.map(task => {
-      if (task.id === taskId) {
-        return { ...task, status: newStatus };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  };
-
-  useEffect(() => {
-    // Change color of tasks in "Done" column using CSS
-    const doneTasks = document.querySelectorAll('.task[data-status="done"]');
-    doneTasks.forEach(taskElement => {
-      taskElement.style.backgroundColor = "#dbf3c9";
-    });
-  }, [tasks]);
-
-  return (
-    <BoardContext.Provider value={{ tasks, moveTask }}>
-      <Board />
-    </BoardContext.Provider>
-  );
-};
-
-const Board = () => {
-  const { tasks } = useContext(BoardContext);
-
-  return (
-    <div className="board">
-      <Column title="To Do 🪄" tasks={tasks.filter(task => task.status === 'todo')} />
-      <Column title="In Progress 🚀" tasks={tasks.filter(task => task.status === 'inProgress')} />
-      <Column title="Done 🌟" tasks={tasks.filter(task => task.status === 'done')} />
-    </div>
-  );
-};
-
-
 
 export default function App() {
   return (
     <div className="App">
-      <TaskBoardProvider />
+      <Link to="/"><img className='logo' src='https://play.nintendo.com/images/PLAY-5075-PBN-Bowser_Logo_v02.e1456275.png' alt='bowser logo' width={200} /></Link>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/one" exact element={<One />} />
+        <Route path="/two" exact element={<Two />} />
+        <Route path="/three" exact element={<Three />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() { 
+    return ( 
+        <div className="center"> 
+         <p>welcome!</p>
+        </div> 
+    ); 
+}
